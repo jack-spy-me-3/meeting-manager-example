b3ae0560b07380984fbfe108d4455a48dbf1e1fd
Samantha Morco <samanthamorco@gmail.com>
2017-03-02 20:08:21 -0500

    add edit and update options

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -37,4 +37,30 @@ class MeetingsController < ApplicationController
       render 'new.html.erb'
     end
   end
+
+  def edit
+    @meeting = Meeting.find_by(id: params[:id])
+    render 'edit.html.erb'
+  end
+
+  def update
+    @meeting = Meeting.find_by(id: params[:id])
+    @meeting.assign_attributes(name: params[:name],
+                               address: params[:address],
+                               start_time: params[:start_time],
+                               end_time: params[:end_time],
+                               notes: params[:notes]
+                              )
+    if @meeting.save
+      @meeting.tags.destroy_all
+      params[:tag_ids].each do |tag_id|
+        MeetingTag.create(meeting_id: @meeting.id, tag_id: tag_id)
+      end
+      flash[:success] = "Meeting has been updated"
+      redirect_to "/meetings/#{@meeting.id}"
+    else
+      flash[:error] = "Meeting has not been updated. Please try again."
+      render "edit.html.erb"
+    end
+  end
 end

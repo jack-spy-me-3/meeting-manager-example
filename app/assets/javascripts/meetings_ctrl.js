944b50d43426cb50b00a97a93bc6fd248c4f894f
Samantha Morco <samanthamorco@gmail.com>
2017-03-02 21:11:18 -0500

    add angular to index page

--- app/assets/javascripts/meetings_ctrl.js
+++ app/assets/javascripts/meetings_ctrl.js
@@ -1,7 +1,12 @@
 /* global angular */
 (function() {
   "use strict";
-  angular.module("app").controller("meetingsCtrl", function($scope) {
+  angular.module("app").controller("meetingsCtrl", function($scope, $http) {
+    $scope.setup = function() {
+      $http.get("/api/v1/meetings.json").then(function(response) {
+        $scope.meetings = response.data;
+      });
+    };
     $scope.message = "Hello World";
   });
 }());
